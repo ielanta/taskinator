d32d2ef9c460c68a701a83996f8d0720f2875ee6
ielanta <vanda2@mail.ru>
2019-06-21 00:37:23 +0300

    Add CommentDeleteTests

--- activity/tests.py
+++ activity/tests.py
@@ -13,9 +13,9 @@ from task.models import Task
 class CommentListTests(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user('user', 'user@test.com', 'Password')
-        self.task, _ = Task.objects.get_or_create()
-        self.url = reverse('comment-list', kwargs={'task_id': self.task.pk})
-        self.data = {'text': 'My Comment', 'task': self.task.pk}
+        task, _ = Task.objects.get_or_create()
+        self.url = reverse('comment-list', kwargs={'task_id': task.pk})
+        self.data = {'text': 'My Comment', 'task': task.pk}
 
     def test_not_authorized(self):
         # check that not authorized users cannot create comment
@@ -40,3 +40,28 @@ class CommentListTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['text'], self.data['text'])
+
+
+class CommentDeleteTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user('user', 'user@test.com', 'Password')
+        task, _ = Task.objects.get_or_create()
+        self.comment, _ = Comment.objects.get_or_create(task=task, user=self.user, text='My text')
+        self.url = reverse('comment-delete', kwargs={'task_id': task.pk, 'pk': self.comment.pk})
+
+    def test_delete(self):
+        # check that unauthorised can't delete comment
+        response = self.client.get(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+        # check that user can't delete comment
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # check that admin can delete comment
+        admin = User.objects.create_superuser('admin', 'admin@test.com', 'password')
+        self.client.force_authenticate(user=admin)
+        response = self.client.delete(self.url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
